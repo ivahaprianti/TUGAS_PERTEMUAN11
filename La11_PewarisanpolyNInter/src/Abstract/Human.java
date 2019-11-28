59fa430ec96e59f61367d99a46d4b51e1f4f8075
ivahaprianti <hidayativah@gmail.com>
2019-11-28 10:48:35 +0700

    TUGAS_PERTEMUAN11

---
+++ La11_PewarisanpolyNInter/src/Abstract/Human.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abstract;
+
+/**
+ *
+ * @author
+ */
+public class Human {
+public static void main(String[] args) {
+}
+   public void walk(){
+ System.out.println("Human walks...");
+ }
+ } 
---
+++ La11_PewarisanpolyNInter/src/Polimorphisme/Student.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polimorphisme;
+
+/**
+ *
+ * @author 
+ */
+public class Student extends Person {
+public Student()
+ {
+ //super( "SomeName", "SomeAddress");
+ //super();
+ //super.name = "name";
+ System.out.println("Inside Student:Constructor");
+ }
+ public String getName() {
+ System.out.println("Student Name : " +name);
+ return name; 
+ }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student anna = new Student();
+    }
+    
+}
